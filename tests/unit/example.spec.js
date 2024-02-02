231b7e7aa28a314f55f0b2293279a2b9be3abb16
mm-saiful6854 <cse.saiful2119@gmail.com>
2024-02-02 18:27:34 +0600

    change unit test file

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import AddTodo from "@/components/AddTodo.vue";
 
-describe('HelloWorld.vue', () => {
+describe('AddTodo.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
+    const msg = 'Add todo'
+    const wrapper = shallowMount(AddTodo)
     expect(wrapper.text()).toMatch(msg)
   })
 })
